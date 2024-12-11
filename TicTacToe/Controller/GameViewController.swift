ee1510b99d5b3f6a9bb13664e0ff1c251b1a0faf
Eliaspd <swedetailer@gmail.com>
2024-12-11 13:20:08 +0100

    Fixed playergesture errors

--- TicTacToe/Controller/GameViewController.swift
+++ TicTacToe/Controller/GameViewController.swift
@@ -11,27 +11,30 @@ class GameViewController: UIViewController {
     @IBOutlet var boardSquares: [UIImageView]!
     @IBOutlet weak var xPlayer: UIImageView!
     @IBOutlet weak var oPlayer: UIImageView!
+    @IBOutlet weak var statusLabel: UILabel!
     
+    var game: TicTacToe!
     var initialXposition: CGPoint = CGPoint.zero
     var initialOposition: CGPoint = CGPoint.zero
     var droppedInSquare = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let playerX = Player(score: 0, userName: "Player X", type: .x)
+        let playerO = Player(score: 0, userName: "Player O", type: .o)
+        game = TicTacToe(playerX: playerX, playerO: playerO)
+        
         initialXposition = xPlayer.center
         initialOposition = oPlayer.center
+        
+        updateStatusLabel()
     }
 
     @IBAction func resetGame(_ sender: UIButton) {
-        let initialSquareImage = UIImage(systemName: "B1")
-        for square in boardSquares {
-            square.image = initialSquareImage
-        }
-        UIView.animate(withDuration: 0.3) {
-            self.xPlayer.center = self.initialXposition
-            self.oPlayer.center = self.initialOposition
-        }
-        droppedInSquare = false
+        game.resetGame()
+        resetBoardUI()
+        updateStatusLabel()
     }
     
     @IBAction func xPlayerGesture(_ sender: UIPanGestureRecognizer) {
@@ -41,12 +44,12 @@ class GameViewController: UIViewController {
     @IBAction func oPlayerGesture(_ sender: UIPanGestureRecognizer) {
         handlePlayerGesture(sender, player: oPlayer, initialPosition: initialOposition)
     }
-    
+
     private func handlePlayerGesture(_ sender: UIPanGestureRecognizer, player: UIImageView, initialPosition: CGPoint) {
         let translation = sender.translation(in: self.view)
         player.center = CGPoint(x: translation.x + player.center.x, y: translation.y + player.center.y)
         sender.setTranslation(.zero, in: self.view)
-        
+
         if sender.state == .ended {
             droppedInSquare = false
             
@@ -55,11 +58,25 @@ class GameViewController: UIViewController {
                 let playerFrame = player.superview?.convert(player.frame, to: self.view) ?? player.frame
                 
                 if squareFrame.contains(playerFrame) {
-                    player.center = square.center
+                    droppedInSquare = true
                     square.image = player.image
                     square.tintColor = player.tintColor
-                    droppedInSquare = true
-                    player.center = initialPosition
+                    
+                    if let index = boardSquares.firstIndex(of: square) {
+                        let row = index / 3
+                        let col = index % 3
+                        if game.makeMove(row: row, col: col) {
+                            updateStatusLabel()
+                            
+                            if game.gameState != .ongoing {
+                                handleGameEnd()
+                            }
+                        }
+                    }
+
+                    UIView.animate(withDuration: 0.3) {
+                        player.center = initialPosition
+                    }
                     break
                 }
             }
@@ -71,4 +88,58 @@ class GameViewController: UIViewController {
             }
         }
     }
+
+    private func resetBoardUI() {
+        let initialSquareImage = UIImage(systemName: "B1")
+        for square in boardSquares {
+            square.image = initialSquareImage
+        }
+        UIView.animate(withDuration: 0.3) {
+            self.xPlayer.center = self.initialXposition
+            self.oPlayer.center = self.initialOposition
+        }
+    }
+    
+    private func updateStatusLabel() {
+        switch game.gameState {
+        case .ongoing:
+            statusLabel.text = "\(game.currentPlayer.userName)'s turn"
+        case .draw:
+            statusLabel.text = "It's a draw!"
+        case .win(let winner):
+            statusLabel.text = "\(winner.userName) wins!"
+        }
+    }
+    private func showWinAlert(winningPlayer: Player) {
+        let alert = UIAlertController(
+            title: "Congratulations!",
+            message: "\(winningPlayer.userName) wins!",
+            preferredStyle: .alert
+        )
+        alert.addAction(UIAlertAction(title: "OK", style: .default))
+        self.present(alert, animated: true)
+    }
+
+    private func showDrawAlert() {
+        let alert = UIAlertController(
+            title: "It's a Draw!",
+            message: "No more moves left!",
+            preferredStyle: .alert
+        )
+        alert.addAction(UIAlertAction(title: "OK", style: .default))
+        self.present(alert, animated: true)
+    }
+    
+    private func handleGameEnd() {
+        switch game.gameState {
+        case .win(let winner):
+            showWinAlert(winningPlayer: winner)
+        case .draw:
+            showDrawAlert()
+        case .ongoing:
+            break
+        }
+    }
+    
+   
 }
